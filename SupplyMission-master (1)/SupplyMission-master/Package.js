45e45573771116959443b97a112b75bb0c80ec14
XxG4laxyCRAFTxX <69312383+XxG4laxyCRAFTxX@users.noreply.github.com>
2020-09-28 19:59:51 +0530

    Add files via upload

---
+++ SupplyMission-master (1)/SupplyMission-master/Package.js
@@ -0,0 +1,12 @@
+class Package{
+    constructor(x, y, width, height){
+        var options = {
+            'isStatic' : false,
+            'restitution' : 0.1,
+            'friction' : 2.3,
+            'density' : 2.3
+        }
+
+        
+    }
+}
